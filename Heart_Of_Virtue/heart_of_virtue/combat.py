051afa839fb9dbce657530108da5796d6ee0aa6e
azureknight63 <basharteg6@gmail.com>
2016-06-27 16:32:48 -0400

    Changes from work

--- Heart_Of_Virtue/heart_of_virtue/combat.py
+++ Heart_Of_Virtue/heart_of_virtue/combat.py
@@ -27,3 +27,10 @@ def combat(player, enemy_list):
                         cprint("You'll need to rest a bit before you can do that.", "red")
                     elif move.cooldown_left > 0:
                         cprint("You're not yet ready to do that again.", "red")
+        for i, enemy in enumerate(enemy_list):
+            if not enemy.is_alive:
+                print(colored(enemy.name, "magenta") + " exploded into fragments of light!")
+                enemy_list.pop([i])
+            else:
+                pass
+
