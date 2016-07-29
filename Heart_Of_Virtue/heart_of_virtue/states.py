d5c79eff7cf74cca0cb7d97d5683b40e2dcb4fc6
azureknight63 <basharteg6@gmail.com>
2016-07-29 09:48:52 -0400

    changes from laptop

--- Heart_Of_Virtue/heart_of_virtue/states.py
+++ Heart_Of_Virtue/heart_of_virtue/states.py
@@ -6,19 +6,21 @@ from termcolor import colored, cprint
 import random
 
 class State: #master class for all states
-    def __init__(self, name, description, beats_max, beats_left, apply_announce, target, source):
+    '''
+    If beats_max is 0 (default), the state will not expire after n beats.
+
+    '''
+    def __init__(self, name, target,
+                 source=None, apply_announce="", description="", beats_max=0):
         self.name = name
         self.description = description  # todo finish this
-
-        self.beats_left = beats_left
+        self.beats_max = beats_max
+        self.beats_left = self.beats_max
         self.apply_announce = apply_announce
 
         self.target = target  # can be the same as the user in abilities with no targets
-        self.user = user
-        self.targeted = targeted  # Is the move targeted at something?
-        self.interrupted = False  # When a move is interrupted, skip all remaining actions for that move, set the
-        # move's cooldown
-        self.initialized = False
+        self.source = source
+
 
     def process_stage(self, user):
         if user.current_move == self:
@@ -78,192 +80,3 @@ class State: #master class for all states
         # print("######{}: I'm in the cooldown stage now".format(self.name)) #debug message
         pass
 
-### PLAYER MOVES ###
-
-class Attack(Move): #basic attack function, always uses equipped weapon, player only
-    def __init__(self, player):
-        description = "Strike at your enemy with your equipped weapon."
-        power = player.eq_weapon.damage + \
-                    (player.strength * player.eq_weapon.str_mod) + \
-                    (player.finesse * player.eq_weapon.fin_mod)
-        prep = 50 / player.speed #starting prep of 5
-        if prep < 1:
-            prep = 1
-        execute = 1
-        recoil = 1 + player.eq_weapon.weight
-        cooldown = 5 - (player.speed/10)
-        if cooldown < 0:
-            cooldown = 0
-        weapon = player.eq_weapon.name
-        fatigue_cost = 100 - (5 * player.endurance)
-        if fatigue_cost <= 10:
-            fatigue_cost = 10
-        super().__init__(name="Attack", description=description, xp_gain=1, heat_gain= 0.1, current_stage=0,
-                         stage_beat=[prep,execute,recoil,cooldown], targeted=True,
-                         stage_announce=["You wind up for a strike...",
-                                         colored("You strike with your " + weapon + "!", "green"),
-                                         "You brace yourself as your weapon recoils.",
-                                         ""], fatigue_cost=fatigue_cost, beats_left=prep,
-                         target=None, user=player)
-        self.power = power
-
-    def execute(self, player):
-        # print("######{}: I'm in the execute stage now".format(self.name)) #debug message
-        print(self.stage_announce[1])
-        hit_chance = (95 - self.target.finesse) + self.user.finesse
-        roll = random.randint(0, 100)
-        damage = ((self.power - self.target.protection) * player.heat) * random.uniform(0.8, 1.2)
-        damage = int(damage)
-        if hit_chance >= roll: #a hit!
-            print(colored(self.target.name, "magenta") + colored(" was struck for ", "yellow") +
-                  colored(damage, "red") + colored(" damage!", "yellow"))
-            self.target.hp -= damage
-            # print("######{}'s HP is {}".format(self.target.name, self.target.hp)) #debug msg
-            player.change_heat(1.25)
-        else:
-            print(colored("Just missed!", "white"))
-            player.change_heat(0.75)
-        self.user.fatigue -= self.fatigue_cost
-
-class Rest(Move):  # standard rest to restore fatigue.
-    def __init__(self, player):
-        description = "Rest for a moment to restore fatigue."
-        prep = 1
-        execute = 1
-        recoil = 2
-        cooldown = 0
-        fatigue_cost = 0
-        super().__init__(name="Rest", description=description, xp_gain=0, heat_gain=0.1, current_stage=0,
-                         targeted=False,
-                         stage_beat=[prep,execute,recoil,cooldown],
-                         stage_announce=["You relax your muscles for a moment.",
-                                         colored("You are resting.", "green"),
-                                         "",
-                                         ""], fatigue_cost=fatigue_cost,
-                         beats_left=execute, target=player, user=player)
-
-    def execute(self, player):
-        print(self.stage_announce[1])
-        recovery_amt = (player.maxfatigue * 0.25) * random.uniform(0.8, 1.2)
-        recovery_amt = int(recovery_amt)
-        if recovery_amt > player.maxfatigue - player.fatigue:
-            recovery_amt = player.maxfatigue - player.fatigue
-        player.fatigue += recovery_amt
-        cprint("You recovered {} FP!".format(recovery_amt), "green")
-
-class Use_Item(Move): #basic attack function, always uses equipped weapon
-    def __init__(self, player):
-        description = "Use an item from your inventory."
-        prep = 1
-        execute = 0
-        recoil = 1
-        cooldown = 0
-        fatigue_cost = 0
-        super().__init__(name="Use Item", description=description, xp_gain=0, heat_gain=0.0, current_stage=0,
-                         targeted=False,
-                         stage_beat=[prep,execute,recoil,cooldown],
-                         stage_announce=["You open your bag.",
-                                         "",
-                                         "You close your bag.",
-                                         ""], fatigue_cost=fatigue_cost,
-                         beats_left=execute, target=player, user=player)
-
-    def execute(self, player):
-        player.use_item() # opens the category view for the standard "use item" action
-
-### NPC MOVES ###
-
-class NPC_Attack(Move): #basic attack function, NPCs only
-    def __init__(self, npc):  #todo Fix this - getting an error; something to do with targeting
-        description = ""
-        power = (npc.damage * random.uniform(0.8, 1.2))
-
-        prep = 50 / npc.speed
-        if prep < 1:
-            prep = 1
-
-        execute = 1
-
-        recoil = 50 / npc.speed
-        if recoil < 0:
-            recoil = 0
-
-        cooldown = 5 - (npc.speed/10)
-        if cooldown < 0:
-            cooldown = 0
-
-        fatigue_cost = 100 - (5 * npc.endurance)
-        if fatigue_cost <= 10:
-            fatigue_cost = 10
-
-        super().__init__(name="NPC_Attack", description=description, xp_gain=1, heat_gain= 0.1, current_stage=0,
-                         stage_beat=[prep,execute,recoil,cooldown], targeted=True,
-                         stage_announce=[colored("{} coils in preparation for an attack!".format(npc.name), "red"),
-                                         colored("{} lashes out at you with extreme violence!".format(npc.name), "red"),
-                                         "{} recoils from the attack.".format(npc.name),
-                                         ""],
-                         fatigue_cost=fatigue_cost, beats_left=prep,
-                         target=npc.target, user=npc)
-        self.power = power
-
-    def execute(self, npc):
-        print(self.stage_announce[1])
-        hit_chance = (95 - self.target.finesse)  # + self.user.finesse
-        roll = random.randint(0, 100)
-        damage = (self.power - self.target.protection)
-        damage = int(damage)
-        if hit_chance >= roll: #a hit!
-            print(colored(self.user.name, "magenta") + colored(" hit you for ", "yellow") +
-                  colored(damage, "red") + colored(" damage!", "yellow"))
-            self.target.hp -= damage
-            self.target.change_heat(1 - (damage / self.target.maxhp))  # reduce heat by the percentage of dmg done to
-            #  maxhp
-        else:
-            print(colored("{}'s attack just missed!".format(self.user.name), "white")) #todo finish this
-        self.user.fatigue -= self.fatigue_cost
-
-class NPC_Rest(Move):  # standard rest to restore fatigue for NPCs.
-    def __init__(self, npc):
-        description = "Rest for a moment to restore fatigue."
-        prep = 0
-        execute = 1
-        recoil = 2
-        cooldown = 0
-        fatigue_cost = 0
-        super().__init__(name="Rest", description=description, xp_gain=0, heat_gain=0.0, current_stage=0,
-                         targeted=False,
-                         stage_beat=[prep,execute,recoil,cooldown],
-                         stage_announce=["{} rests for a moment.".format(npc.name),
-                                         colored("{} is resting.".format(npc.name), "white"),
-                                         "",
-                                         ""], fatigue_cost=fatigue_cost,
-                         beats_left=execute, target=npc, user=npc)
-
-    def execute(self, npc):
-        print(self.stage_announce[1])
-        recovery_amt = (self.user.maxfatigue * 0.25) * random.uniform(0.8, 1.2)
-        recovery_amt = int(recovery_amt)
-        if recovery_amt > self.user.maxfatigue - self.user.fatigue:
-            recovery_amt = self.user.maxfatigue - self.user.fatigue
-        self.user.fatigue += recovery_amt
-
-
-class NPC_Idle(Move):  # NPC does nothing for a few beats.
-    def __init__(self, npc):
-        description = "What?"
-        prep = 0
-        execute = 3
-        recoil = 0
-        cooldown = 0
-        fatigue_cost = 0
-        super().__init__(name="Idle", description=description, xp_gain=0, heat_gain=0.0, current_stage=0,
-                         targeted=False,
-                         stage_beat=[prep,execute,recoil,cooldown],
-                         stage_announce=["",
-                                         str(npc.name + npc.idle_message),
-                                         "",
-                                         ""], fatigue_cost=fatigue_cost,
-                         beats_left=execute, target=npc, user=npc)
-
-    def execute(self, npc):
-        print(self.stage_announce[1])
