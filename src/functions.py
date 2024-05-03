8796ae8f65fc71da26974755c2af7dfae4136fc5
azureknight63 <azureknight63@hotmail.com>
2024-05-03 13:28:18 -0400

    Optimize file and sequester interaction search to functions.py

--- src/functions.py
+++ src/functions.py
@@ -20,6 +20,72 @@ def print_slow(text, speed=1):
         time.sleep(rate)
 
 
+def confirm(thing, action, context):
+    player = context[0]
+    count_args = context[1]
+    check = input(colored("{} {}? (y/n)".format(count_args[0].title(), thing.name), "cyan"))
+    if check.lower() == ('y' or 'yes'):
+        thing.__getattribute__(action)(player)
+        return True
+    else:
+        return False
+
+
+def enumerate_for_interactions(subjects, context):
+    player = context[0]
+    count_args = context[1]
+    action_input = context[2]
+    if len(count_args) == 1:
+        candidates = []
+        for thing in subjects:
+            if hasattr(thing, "keywords"):
+                if not thing.hidden:
+                    for keyword in thing.keywords:
+                        if action_input == keyword:
+                            candidates.append(thing)
+                        # if action_input == keyword and confirm(thing, keyword):
+                        #     return True
+            elif hasattr(thing, "interactions"):
+                for interaction in thing.interactions:
+                    if action_input == interaction:
+                        candidates.append(thing)
+                    # if action_input == interaction and confirm(thing, interaction):
+                    #    return True
+        if len(candidates) == 1:  # if there is only one possibility, skip the confirmation step
+            candidates[0].__getattribute__(action_input)(player)
+            return True
+        elif candidates:  # otherwise, if there is more than one possibility, ask the player to confirm
+            for candidate in candidates:
+                if confirm(candidate, action_input, context):
+                    return True
+    elif len(count_args) > 1:
+        candidates = []
+        for i, thing in enumerate(subjects):
+            if hasattr(thing, "keywords"):
+                search_item = thing.name.lower() + ' ' + thing.idle_message.lower()
+                if count_args[1] in search_item and not thing.hidden:
+                    for keyword in thing.keywords:
+                        if count_args[0] == keyword:
+                            candidates.append(thing)
+                        # if count_args[0] == keyword and confirm(thing, keyword):
+                        #    return True
+            elif hasattr(thing, "interactions"):
+                search_item = thing.name.lower() + ' ' + thing.description.lower() + ' ' + thing.announce.lower()
+                if count_args[1] in search_item and not thing.hidden:
+                    for interaction in thing.interactions:
+                        if count_args[0] == interaction:
+                            candidates.append(thing)
+                        # if count_args[0] == interaction and confirm(thing, interaction):
+                        #    return True
+        if len(candidates) == 1:  # if there is only one possibility, skip the confirmation step
+            candidates[0].__getattribute__(count_args[0])(player)
+            return True
+        elif candidates:  # otherwise, if there is more than one possibility, ask the player to confirm
+            for candidate in candidates:
+                if confirm(candidate, count_args[0], context):
+                    return True
+    return False
+
 def screen_clear():
     print("\n" * 100)
 
--- src/game.py
+++ src/game.py
@@ -117,18 +117,20 @@ _\\|//__( | )______)_/
         items.get_all_subtypes()  # creates the 'All' archetypes for each item group; used for states/item effects/etc.
         ### enter post-menu game loop ###
         while player.is_alive() and not player.victory and not player.main_menu:
-            for item in player.inventory:
-                item.owner = player  # enforce player ownership of all inventory items; used for special interactions
-            if not player.eq_weapon:  # if the player is unarmed, "equip" fists
-                player.eq_weapon = player.fists
-            player.time_elapsed += (time.time() - check_time)
-            auto_save_timer += (time.time() - check_time)
+            elapsed_time = time.time() - check_time
+            player.time_elapsed += elapsed_time
+            auto_save_timer += elapsed_time
             if auto_save_timer > 3000:  # autosave timer
                 functions.autosave(player)
                 auto_save_timer = 0
             check_time = time.time()
-            room = player.universe.tile_exists(player.map, player.location_x, player.location_y)
-            player.current_room = room
+
+            for item in player.inventory:
+                item.owner = player  # enforce player ownership of all inventory items; used for special interactions
+            if not player.eq_weapon:  # if the player is unarmed, "equip" fists
+                player.eq_weapon = player.fists
+
+            player.current_room = player.universe.tile_exists(player.map, player.location_x, player.location_y)
             if player.universe.game_tick > 0:
                 player.current_room.last_entered = player.universe.game_tick
             else:
@@ -136,15 +138,15 @@ _\\|//__( | )______)_/
             player.recall_friends()  # bring any party members along
             for actor in player.combat_list_allies:
                 actor.cycle_states()
-            room.evaluate_events()
-            room.modify_player(player)
+            player.current_room.evaluate_events()
+            player.current_room.modify_player(player)
             if mark_health != player.hp:
                 player.show_bars(True, False)  # show just the health bar if the player's current HP has changed
                 mark_health = player.hp
-            functions.print_npcs_in_room(room)
-            room.stack_duplicate_items()
-            functions.print_items_in_room(room)
-            functions.print_objects_in_room(room)
+            functions.print_npcs_in_room(player.current_room)
+            player.current_room.stack_duplicate_items()
+            functions.print_items_in_room(player.current_room)
+            functions.print_objects_in_room(player.current_room)
             player.combat_list = functions.check_for_combat(player)
             if len(player.combat_list) > 0:  # Check the state of the room to see if there are any enemies
                 print(colored("Jean readies himself for battle!", "red"))
@@ -152,14 +154,13 @@ _\\|//__( | )______)_/
             ### check to make sure entering the most recent tile hasn't ended the game ###
             if not player.is_alive():
                 player.death()
-            if player.is_alive() and not player.victory:
+            elif player.is_alive() and not player.victory:
                 player.stack_inv_items()
                 player.stack_gold()
                 print("\nChoose an action:\n")
-                available_actions = room.adjacent_moves()
-                available_moves = colored('| ', "cyan")
-                for action in available_actions:
-                    available_moves += (colored(str(action), "green")) + colored(' | ', "cyan")
+                available_actions = player.current_room.adjacent_moves()
+                move_separator = colored(' | ', "cyan")
+                available_moves = move_separator.join(colored(str(action), "green") for action in available_actions)
                 while available_moves.count('|') > 4:  # Break the list of moves over multiple lines
                     cutoff = functions.findnth(available_moves, "|", 4)
                     print(available_moves[:cutoff + 1])
@@ -168,14 +169,14 @@ _\\|//__( | )______)_/
                         available_moves = ""
                 if len(available_moves) > 0:
                     print(available_moves)
-                print("\n\nFor a list of additional commands, enter 'c'.\n")
+                print("\nFor a list of additional commands, enter 'c'.\n")
                 action_input = input('Action: ')
                 raw_args = action_input.split(' ')
                 lc_exceptions = ("te", "test",
                                  "testevent")  # exceptions to lowering case (better precision, worse searching, may break some actions)
                 if raw_args[0] not in lc_exceptions:
                     action_input = action_input.lower()
-                available_actions = room.available_actions()
+                available_actions = player.current_room.available_actions()
                 count_args = action_input.split(' ')
                 arbitrary_action = True  # this will be set to False if the action is a default one that the player
                 #                          normally has access to
@@ -195,82 +196,21 @@ _\\|//__( | )______)_/
                                 break
                 if arbitrary_action:  # if the command the player used could not be found in the list of default
                     #                   actions, check to see if objects in the room have an associated command.
-                    #                   NOTE: in arbitrary one-word commands, ALL objects that associate the command
+                    #                   In arbitrary one-word commands, ALL objects that associate the command
                     #                   will evaluate their respective functions
-                    #                   NOTE: syntax for multiple words is '<command> <target object>';
+                    #                   The syntax for multiple words is '<command> <target object>';
                     #                   additional words are ignored
+                    interaction_scopes = [
+                        player.current_room.objects_here + player.current_room.npcs_here,
+                        player.inventory
+                        ]
                     success = False
-
-                    def enumerate_for_interactions(subjects):
-
-                        def confirm(thing, action):
-                            check = input(colored("{} {}? (y/n)".format(count_args[0].title(), thing.name), "cyan"))
-                            if check.lower() == ('y' or 'yes'):
-                                thing.__getattribute__(action)(player)
-                                return True
-                            else:
-                                return False
-
-                        if len(count_args) == 1:
-                            candidates = []
-                            for thing in subjects:
-                                if hasattr(thing, "keywords"):
-                                    if not thing.hidden:
-                                        for keyword in thing.keywords:
-                                            if action_input == keyword:
-                                                candidates.append(thing)
-                                            # if action_input == keyword and confirm(thing, keyword):
-                                            #     return True
-                                elif hasattr(thing, "interactions"):
-                                    for interaction in thing.interactions:
-                                        if action_input == interaction:
-                                            candidates.append(thing)
-                                        #if action_input == interaction and confirm(thing, interaction):
-                                        #    return True
-                            if len(candidates) == 1:  #if there is only one possibility, skip the confirmation step
-                                candidates[0].__getattribute__(action_input)(player)
-                                return True
-                            elif candidates:  #otherwise, if there is more than one possibility, ask the player to confirm
-                                for candidate in candidates:
-                                    if confirm(candidate, action_input):
-                                        return True
-                        elif len(count_args) > 1:
-                            candidates = []
-                            for i, thing in enumerate(subjects):
-                                if hasattr(thing, "keywords"):
-                                    search_item = thing.name.lower() + ' ' + thing.idle_message.lower()
-                                    if count_args[1] in search_item and not thing.hidden:
-                                        for keyword in thing.keywords:
-                                            if count_args[0] == keyword:
-                                                candidates.append(thing)
-                                            #if count_args[0] == keyword and confirm(thing, keyword):
-                                            #    return True
-                                elif hasattr(thing, "interactions"):
-                                    search_item = thing.name.lower() + ' ' + thing.description.lower() + ' ' + thing.announce.lower()
-                                    if count_args[1] in search_item and not thing.hidden:
-                                        for interaction in thing.interactions:
-                                            if count_args[0] == interaction:
-                                                candidates.append(thing)
-                                            #if count_args[0] == interaction and confirm(thing, interaction):
-                                            #    return True
-                            if len(candidates) == 1:  #if there is only one possibility, skip the confirmation step
-                                candidates[0].__getattribute__(count_args[0])(player)
-                                return True
-                            elif candidates:  #otherwise, if there is more than one possibility, ask the player to confirm
-                                for candidate in candidates:
-                                    if confirm(candidate, count_args[0]):
-                                        return True
-                        return False
-
-                    subjects = room.objects_here + room.npcs_here
-                    success = enumerate_for_interactions(subjects)
-                    if not success:  # Now check items in the player's inventory
-                        subjects = player.inventory
-                        success = enumerate_for_interactions(subjects)
-
+                    for scope in interaction_scopes:
+                        if functions.enumerate_for_interactions(scope, (player, count_args, action_input)):
+                            success = True  # Check each scope separately and, if an interaction is found, don't check the remaining scope(s)
+                            break
                     if not success:  # Nothing was found matching the arbitrary input, so Jean is mightily confused
                         cprint("Jean isn't sure exactly what he's trying to do.", 'red')
-
             time.sleep(0.5)
 
 
